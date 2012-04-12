6265efb814d54a034e2f0d2734a0511bd2755e9e
ezku <eevert.saukkokoski@brainalliance.com>
2012-04-12 19:26:30 +0300

    added motivational poster

--- js/app.js
+++ js/app.js
@@ -1,32 +1,4 @@
 $(document).ready(function() {
-	/* Skeleton tabs activation
-	================================================== */
-	var tabs = $('ul.tabs');
-	
-	tabs.each(function(i) {
-		//Get all tabs
-		var tab = $(this).find('> li > a');
-		tab.click(function(e) {
-			
-			//Get Location of tab's content
-			var contentLocation = $(this).attr('href') + "Tab";
-			
-			//Let go if not a hashed one
-			if(contentLocation.charAt(0)=="#") {
-			
-				e.preventDefault();
-			
-				//Make Tab Active
-				tab.removeClass('active');
-				$(this).addClass('active');
-				
-				//Show Tab Content & add active class
-				$(contentLocation).show().addClass('active').siblings().hide().removeClass('active');
-				
-			} 
-		});
-	});
-	
 	/* Background noise
 	================================================== */
 	$('body').noisy({
